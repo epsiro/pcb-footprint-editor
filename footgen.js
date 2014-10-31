3f246958c1f834f7c7330751acca0ee298089ef6
Bendik S. Søvegjarto <bendik@sovegjarto.com>
2014-10-31 09:55:04 +0100

    moved pad size calculation to Pad method, and moved pad parsing to own function

--- footgen.js
+++ footgen.js
@@ -18,6 +18,25 @@ $( "#zoom_slider" ).slider({
     }
 });
 
+function parse_pad_line(line) {
+    if ( ! line.match(/Pad/)) {
+        throw new UserException("InvalidFormat");
+    }
+
+    pad_line = line.substring(line.indexOf('[') + 1).split(' ');
+
+    //console.log(pad_line);
+
+    var x1 = pad_line[0].slice(0, -2)*100;
+    var y1 = pad_line[1].slice(0, -2)*100;
+    var x2 = pad_line[2].slice(0, -2)*100;
+    var y2 = pad_line[3].slice(0, -2)*100;
+    var thickness = pad_line[4].slice(0, -2)*100;
+
+    return {x1:x1, y1:y1, x2:x2, y2:y2, thickness:thickness}
+
+}
+
 var editor = CodeMirror.fromTextArea(document.getElementById("footprint_code"), {
   lineNumbers: false,
   mode: "text/html",
@@ -39,66 +58,16 @@ editor.on("change", function(cm){
     for (var i = 0; i < lines.length; i++) {
 
         if (lines[i].match(/Pad/)) {
-            pad_line = lines[i].substring(lines[i].indexOf('[') + 1).split(' ');
 
-            //console.log(pad_line);
+            values = parse_pad_line(lines[i]);
 
-            var x1 = pad_line[0].slice(0, -2)*100;
-            var y1 = pad_line[1].slice(0, -2)*100;
-            var x2 = pad_line[2].slice(0, -2)*100;
-            var y2 = pad_line[3].slice(0, -2)*100;
-            var thickness = pad_line[4].slice(0, -2)*100;
+            pads[0].x1 = values.x1;
+            pads[0].y1 = values.y1;
+            pads[0].x2 = values.x2;
+            pads[0].y2 = values.y2;
+            pads[0].thickness = values.thickness;
+            pads[0].draw();
 
-            //console.log(x1,y1,x2,y2,thickness);
-
-            var line_length = Math.sqrt( Math.pow(x2-x1,2) + Math.pow(y2-y1,2) );
-            var angle = Math.atan2( (y2 - y1), (x2 - x1) ) * (180/Math.PI);
-
-            //console.log(line_length, angle);
-
-            if (angle == 0) {
-                var x = x1 - thickness/2;
-                var y = y1 - thickness/2;
-                var width = line_length + thickness;
-                var height = thickness;
-            } else if (angle == -90) {
-                var x = x2 - thickness/2;
-                var y = y2 - thickness/2;
-                var width = thickness;
-                var height = line_length + thickness;
-            } else if (angle == 90) {
-                var x = x1 - thickness/2;
-                var y = y1 - thickness/2;
-                var width = thickness;
-                var height = line_length + thickness;
-            } else if (angle == 180) {
-                var x = x2 - thickness/2;
-                var y = y2 - thickness/2;
-                var width = line_length + thickness;
-                var height = thickness;
-            } else {
-                console.log("Rotated pad is not supported.");
-                return;
-            }
-
-            //console.log(x,y,height,width);
-
-            if (height < 0 || width < 0) {
-                console.log("Not valid pad.");
-                return;
-            }
-
-            pads[0].pad.attr({x: x});
-            pads[0].pad.attr({y: y});
-            pads[0].pad.attr({height: height});
-            pads[0].pad.attr({width: width});
-
-            pads[0].pad_line_ref.attr({x1: x1});
-            pads[0].pad_line_ref.attr({y1: y1});
-            pads[0].pad_line_ref.attr({x2: x2});
-            pads[0].pad_line_ref.attr({y2: y2});
-
-            console.log("Pad updated with attributes " + pads[0].pad.attr());
         }
     }
 });
@@ -235,6 +204,12 @@ function Pad(pad_number) {
 
     this.pad_number = pad_number;
 
+    this.x1 = 0;
+    this.y1 = 0;
+    this.x2 = 0;
+    this.y2 = 0;
+    this.thickness = 0;
+
     this.pad = paper.rect(100, -100, 60, 60).attr({
         fill: "#8c96a0"
     });
@@ -250,6 +225,63 @@ function Pad(pad_number) {
     this.pad_group = paper.group(this.pad, this.pad_line_ref);
     this.pad_group.attr({class: "pad"});
 }
+
+Pad.prototype.draw = function() {
+    var x1 = this.x1;
+    var y1 = this.y1;
+    var x2 = this.x2;
+    var y2 = this.y2;
+    var thickness = this.thickness;
+
+    var line_length = Math.sqrt( Math.pow(x2-x1,2) + Math.pow(y2-y1,2) );
+    var angle = Math.atan2( (y2 - y1), (x2 - x1) ) * (180/Math.PI);
+
+    //console.log(line_length, angle);
+
+    if (angle == 0) {
+        var x = x1 - thickness/2;
+        var y = y1 - thickness/2;
+        var width = line_length + thickness;
+        var height = thickness;
+    } else if (angle == -90) {
+        var x = x2 - thickness/2;
+        var y = y2 - thickness/2;
+        var width = thickness;
+        var height = line_length + thickness;
+    } else if (angle == 90) {
+        var x = x1 - thickness/2;
+        var y = y1 - thickness/2;
+        var width = thickness;
+        var height = line_length + thickness;
+    } else if (angle == 180) {
+        var x = x2 - thickness/2;
+        var y = y2 - thickness/2;
+        var width = line_length + thickness;
+        var height = thickness;
+    } else {
+        console.log("Rotated pad is not supported.");
+        return;
+    }
+
+    //console.log(x,y,height,width);
+
+    if (height < 0 || width < 0) {
+        console.log("Not valid pad.");
+        return;
+    }
+
+    this.pad.attr({x: x});
+    this.pad.attr({y: y});
+    this.pad.attr({height: height});
+    this.pad.attr({width: width});
+
+    this.pad_line_ref.attr({x1: x1});
+    this.pad_line_ref.attr({y1: y1});
+    this.pad_line_ref.attr({x2: x2});
+    this.pad_line_ref.attr({y2: y2});
+
+    console.log("Pad updated with attributes " + this.pad.attr());
+};
 
 var paper = Snap("#svg");
 
