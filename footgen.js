a287d28c284c4feb955e4d45727d10c411e06549
Bendik S. Søvegjarto <bendik@sovegjarto.com>
2014-11-01 03:14:15 +0100

    multiple pads is shown and may be edited in text editor

--- footgen.js
+++ footgen.js
@@ -43,7 +43,7 @@ var editor = CodeMirror.fromTextArea(document.getElementById("footprint_code"), 
   vimMode: true
 });
 
-editor.on("beforeChange", function (cm, change) {
+editor.on("change", function(cm, change){
 
     /* Only update pad if it has been changed by the code and not by the
      * graphical interface */
@@ -53,172 +53,36 @@ editor.on("beforeChange", function (cm, change) {
         }
     }
 
-    var cursor = cm.getCursor();
-    values = parse_pad_line(cm.getLine(cursor.line));
+    if (change.to.line == change.from.line) {
+        var changed_line = editor.getLine(change.to.line);
 
-    console.log("values ", values);
-});
+        if (changed_line.match(/Pad/)) {
 
-editor.on("change", function(cm){
+            values = parse_pad_line(changed_line);
 
-    /* Only update pad if it has been changed by the code and not by the
-     * graphical interface */
-    for (var i = 0; i < pads.length; i++) {
-        if(pads[i].pad.dragging == true) {
-            return;
-        }
-    }
-
-    var lines = cm.getValue().split(/\r?\n/);
-
-    for (var i = 0; i < lines.length; i++) {
-
-        if (lines[i].match(/Pad/)) {
-
-            values = parse_pad_line(lines[i]);
-
-            pads[0].x1 = values.x1;
-            pads[0].y1 = values.y1;
-            pads[0].x2 = values.x2;
-            pads[0].y2 = values.y2;
-            pads[0].thickness = values.thickness;
-            pads[0].draw();
-
+            for (var i = 0; i < pads.length; i++) {
+                if (pads[i].line_number == change.to.line) {
+                    pads[i].x1 = values.x1;
+                    pads[i].y1 = values.y1;
+                    pads[i].x2 = values.x2;
+                    pads[i].y2 = values.y2;
+                    pads[i].thickness = values.thickness;
+                    pads[i].draw();
+                }
+            }
         }
     }
 });
 
 
-var move_start = function() {
 
-    /* Save some values we start with */
-    this.ox = parseInt(this.attr('x'), 10);
-    this.oy = parseInt(this.attr('y'), 10);
-    this.ow = parseInt(this.attr('width'), 10);
-    this.oh = parseInt(this.attr('height'), 10);
-
-    this.dragging = true;
-};
-
-var move_pad = function(dx, dy, posx, posy) {
-
-    var grid = 10;
-
-    /* Inspect cursor to determine which resize/move process to use */
-    switch (this.attr('cursor')) {
-
-        case 'sw-resize':
-            this.attr({
-                x:      this.ox + Snap.snapTo(grid, dx/zoom_level, 30),
-                y:      this.oy - Snap.snapTo(grid, dy/zoom_level, 30),
-                width:  this.ow - Snap.snapTo(grid, dx/zoom_level, 30),
-                height: this.oh + Snap.snapTo(grid, dy/zoom_level, 30)
-            });
-            break;
-
-        case 'se-resize':
-            this.attr({
-                y:      this.oy - Snap.snapTo(grid, dy/zoom_level, 30),
-                width:  this.ow + Snap.snapTo(grid, dx/zoom_level, 30),
-                height: this.oh + Snap.snapTo(grid, dy/zoom_level, 30)
-            });
-            break;
-
-        case 'ne-resize':
-            this.attr({
-                width:  this.ow + Snap.snapTo(grid, dx/zoom_level, 30),
-                height: this.oh - Snap.snapTo(grid, dy/zoom_level, 30)
-            });
-            break;
-
-        case 'nw-resize':
-            this.attr({
-                x:      this.ox + Snap.snapTo(grid, dx/zoom_level, 30),
-                width:  this.ow - Snap.snapTo(grid, dx/zoom_level, 30),
-                height: this.oh - Snap.snapTo(grid, dy/zoom_level, 30)
-            });
-            break;
-
-        default :
-            this.attr({
-                x:      this.ox + Snap.snapTo(grid, dx/zoom_level, 30),
-                y:      this.oy - Snap.snapTo(grid, dy/zoom_level, 30)
-            });
-            break;
-    }
-
-    if (parseInt(this.attr('width'), 10) < parseInt(this.attr('height'), 10)) {
-        var thickness = (this.attr('width')/100);
-
-        var x1 = this.attr('x')/100 + thickness/2;
-        var y1 = this.attr('y')/100 + thickness/2;
-        var x2 = x1;
-        var y2 = y1 + this.attr('height')/100 - thickness;
-
-    } else {
-        /* Laying down */
-        var thickness = (this.attr('height')/100);
-
-        var x1 = this.attr('x')/100 + thickness/2;
-        var y1 = this.attr('y')/100 + thickness/2;
-
-        var x2 = x1 + this.attr('width')/100 - thickness;
-        var y2 = y1;
-    }
-
-    var element_header = 'Element["" "0805" "0805" "" 1000 1000 -1.5mm -2.5mm 0 100 ""]\n(\n';
-    var pad_code = sprintf("    Pad[%.2fmm %.2fmm %.2fmm %.2fmm %.2fmm 0.6mm 1.2mm \"\" \"1\" \"square\"]", x1, y1, x2, y2, thickness);
-    var element_end = '\n)';
-
-    editor.setValue(element_header + pad_code + element_end);
-
-    pads[0].pad_line_ref.attr({x1: x1*100});
-    pads[0].pad_line_ref.attr({y1: y1*100});
-    pads[0].pad_line_ref.attr({x2: x2*100});
-    pads[0].pad_line_ref.attr({y2: y2*100});
-};
-
-var move_end = function() {
-    this.dragging = false;
-};
-
-var change_cursor = function(e, mouse_x, mouse_y) {
-
-    /* Don't change cursor during a drag operation */
-    if (this.dragging === true) {
-        return;
-    }
-
-    /* X,Y Coordinates relative to shape's orgin */
-    var relative_x =  mouse_x - $('#svg').offset().left - this.attr('x')*zoom_level - origin_x;
-    var relative_y = -mouse_y - $('#svg').offset().top  - this.attr('y')*zoom_level + origin_y;
-
-    var shape_width = this.attr('width')*zoom_level;
-    var shape_height = this.attr('height')*zoom_level;
-
-    var resize_border = 10;
-
-    /* Change cursor */
-    if (       relative_x <               resize_border && relative_y <                resize_border) {
-        this.attr('cursor', 'sw-resize');
-
-    } else if (relative_x > shape_width - resize_border && relative_y <                resize_border) {
-        this.attr('cursor', 'se-resize');
-
-    } else if (relative_x > shape_width - resize_border && relative_y > shape_height - resize_border) {
-        this.attr('cursor', 'ne-resize');
-
-    } else if (relative_x <               resize_border && relative_y > shape_height - resize_border) {
-        this.attr('cursor', 'nw-resize');
-
-    } else {
-        this.attr('cursor', 'move');
-    }
-};
-
-function Pad(pad_number) {
+function Pad(pad_number, line_number) {
 
     this.pad_number = pad_number;
+    this.line_number = line_number;
+
+
+    var parentThis = this;
 
     this.x1 = 0;
     this.y1 = 0;
@@ -235,12 +99,145 @@ function Pad(pad_number) {
         strokeWidth: 2
     });
 
+    pad_code = sprintf("    Pad[%.2fmm %.2fmm %.2fmm %.2fmm %.2fmm 0.6mm 1.2mm \"\" \"1\" \"square\"]\n", this.x1, this.y1, this.x2, this.y2, this.thickness);
+    editor.replaceRange(pad_code, {line: this.line_number, ch: 0});
+
+    var move_start = function() {
+
+        /* Save some values we start with */
+        this.ox = parseInt(this.attr('x'), 10);
+        this.oy = parseInt(this.attr('y'), 10);
+        this.ow = parseInt(this.attr('width'), 10);
+        this.oh = parseInt(this.attr('height'), 10);
+
+        this.dragging = true;
+    };
+
+    var move_pad = function(dx, dy, posx, posy) {
+
+        var grid = 10;
+
+        /* Inspect cursor to determine which resize/move process to use */
+        switch (this.attr('cursor')) {
+
+            case 'sw-resize':
+                this.attr({
+                    x:      this.ox + Snap.snapTo(grid, dx/zoom_level, 30),
+                    y:      this.oy - Snap.snapTo(grid, dy/zoom_level, 30),
+                    width:  this.ow - Snap.snapTo(grid, dx/zoom_level, 30),
+                    height: this.oh + Snap.snapTo(grid, dy/zoom_level, 30)
+                });
+                break;
+
+            case 'se-resize':
+                this.attr({
+                    y:      this.oy - Snap.snapTo(grid, dy/zoom_level, 30),
+                    width:  this.ow + Snap.snapTo(grid, dx/zoom_level, 30),
+                    height: this.oh + Snap.snapTo(grid, dy/zoom_level, 30)
+                });
+                break;
+
+            case 'ne-resize':
+                this.attr({
+                    width:  this.ow + Snap.snapTo(grid, dx/zoom_level, 30),
+                    height: this.oh - Snap.snapTo(grid, dy/zoom_level, 30)
+                });
+                break;
+
+            case 'nw-resize':
+                this.attr({
+                    x:      this.ox + Snap.snapTo(grid, dx/zoom_level, 30),
+                    width:  this.ow - Snap.snapTo(grid, dx/zoom_level, 30),
+                    height: this.oh - Snap.snapTo(grid, dy/zoom_level, 30)
+                });
+                break;
+
+            default :
+                this.attr({
+                    x:      this.ox + Snap.snapTo(grid, dx/zoom_level, 30),
+                    y:      this.oy - Snap.snapTo(grid, dy/zoom_level, 30)
+                });
+                break;
+        }
+
+        if (parseInt(this.attr('width'), 10) < parseInt(this.attr('height'), 10)) {
+            parentThis.thickness = (parseInt(this.attr('width'),10)/100);
+
+            parentThis.x1 = parseInt(this.attr('x'),10)/100 + parentThis.thickness/2;
+            parentThis.y1 = parseInt(this.attr('y'),10)/100 + parentThis.thickness/2;
+            parentThis.x2 = parentThis.x1;
+            parentThis.y2 = parentThis.y1 + parseInt(this.attr('height'),10)/100 - parentThis.thickness;
+
+        } else {
+            /* Laying down */
+            parentThis.thickness = (parseInt(this.attr('height'),10)/100);
+
+            parentThis.x1 = parseInt(this.attr('x'),10)/100 + parentThis.thickness/2;
+            parentThis.y1 = parseInt(this.attr('y'),10)/100 + parentThis.thickness/2;
+            parentThis.x2 = parentThis.x1 + parseInt(this.attr('width'),10)/100 - parentThis.thickness;
+            parentThis.y2 = parentThis.y1;
+        }
+
+        parentThis.pad_line_ref.attr({x1: parentThis.x1*100});
+        parentThis.pad_line_ref.attr({y1: parentThis.y1*100});
+        parentThis.pad_line_ref.attr({x2: parentThis.x2*100});
+        parentThis.pad_line_ref.attr({y2: parentThis.y2*100});
+
+        update_editor();
+    };
+
+    var move_end = function() {
+        this.dragging = false;
+    };
+
+    var change_cursor = function(e, mouse_x, mouse_y) {
+
+        /* Don't change cursor during a drag operation */
+        if (this.dragging === true) {
+            return;
+        }
+
+        /* X,Y Coordinates relative to shape's orgin */
+        var relative_x =  mouse_x - $('#svg').offset().left - this.attr('x')*zoom_level - origin_x;
+        var relative_y = -mouse_y - $('#svg').offset().top  - this.attr('y')*zoom_level + origin_y;
+
+        var shape_width = this.attr('width')*zoom_level;
+        var shape_height = this.attr('height')*zoom_level;
+
+        var resize_border = 10;
+
+        /* Change cursor */
+        if (       relative_x <               resize_border && relative_y <                resize_border) {
+            this.attr('cursor', 'sw-resize');
+
+        } else if (relative_x > shape_width - resize_border && relative_y <                resize_border) {
+            this.attr('cursor', 'se-resize');
+
+        } else if (relative_x > shape_width - resize_border && relative_y > shape_height - resize_border) {
+            this.attr('cursor', 'ne-resize');
+
+        } else if (relative_x <               resize_border && relative_y > shape_height - resize_border) {
+            this.attr('cursor', 'nw-resize');
+
+        } else {
+            this.attr('cursor', 'move');
+        }
+    };
+
     this.pad.mousemove(change_cursor);
     this.pad.drag(move_pad, move_start, move_end);
 
     this.pad_group = paper.group(this.pad, this.pad_line_ref);
     this.pad_group.attr({class: "pad"});
+
+
+    function update_editor() {
+        pad_code = sprintf("    Pad[%.2fmm %.2fmm %.2fmm %.2fmm %.2fmm 0.6mm 1.2mm \"\" \"1\" \"square\"]", parentThis.x1, parentThis.y1, parentThis.x2, parentThis.y2, parentThis.thickness);
+        editor.replaceRange(pad_code, {line: parentThis.line_number, ch: 0}, {line: parentThis.line_number, ch: editor.getLine(parentThis.line_number).length});
+    };
+
 }
+
 
 Pad.prototype.draw = function() {
     var x1 = this.x1;
@@ -337,12 +334,17 @@ zoom_group.transform("translate(" + origin_x + "," + origin_y + ") scale(" + zoo
 
 
 var add_pad = function() {
-    pad_instance = new Pad(pads.length);
+    pad_instance = new Pad(pads.length, editor.lineCount() - 1);
     zoom_group.add(pad_instance.pad_group);
     pads.push(pad_instance);
     //console.log(pads);
 }
 
 $(document).bind('keydown', 'p', add_pad);
+
+var element_header = 'Element["" "0805" "0805" "" 1000 1000 -1.5mm -2.5mm 0 100 ""]\n(\n';
+var element_end = ')';
+editor.setValue(element_header + element_end);
+
 
 })();
