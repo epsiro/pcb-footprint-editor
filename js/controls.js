7209ecbb038f97cd8a616ede1aa5463e0a02fc97
Bendik S. Søvegjarto <bendik@sovegjarto.com>
2014-11-14 22:36:59 +0100

    one click upload

--- js/controls.js
+++ js/controls.js
@@ -19,7 +19,8 @@ $("#controls_undo").on("click", function() { editor.undo(); });
 $("#controls_redo").on("click", function() { editor.redo(); });
 
 $("#controls_new_component").on("click", new_component);
-$("#load_file").on("click", load_file_as_text);
+$("#load_file").on("click", function() { $("#file_to_load").click(); });
+$("#file_to_load").on("change", load_file_as_text);
 $("#save_file").on("click", save_text_as_file);
 
 $("#status_xy_distance").parent().hide();
